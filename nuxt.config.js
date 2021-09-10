c0d6440bdc466db24ddfc84cd2d406f0dd4dfc1e
Hiroki Ohara <latelucet@gmail.com>
2021-09-10 23:25:22 +0900

    change logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/svg', href: '/favicon.svg' }
     ]
   },
 
